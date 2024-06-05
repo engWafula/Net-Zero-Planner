7c91d3f7caa765cd173dab9dda9d3af83e908dc4
engWafula <wafulaallan5@gmail.com>
2024-06-05 07:58:42 +0300

    adding auth pages

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,4 @@
-// import MobileNav from "@/components/MobileNav";
-// import Sidebar from "@/components/Sidebar";
-// import { getLoggedInUser } from "@/lib/actions/user.actions";
+
 import { Main } from "next/document";
 import Image from "next/image";
 import { redirect } from "next/navigation";
@@ -19,14 +17,9 @@ export default   async function RootLayout({
 
     return (
       <main className="flex h-screen w-full font-inter">
-          {/* <Sidebar user={loggedIn} /> */}
           <div className="flex size-full flex-col">
         <div className="root-layout">
-          {/* <Image src="/icons/logo.svg" width={30} height={30} alt="logo" /> */}
-          <div>
-            {/* <MobileNav
-             user={loggedIn} />  */}
-          </div>
+
         </div>
         {children}
       </div>
